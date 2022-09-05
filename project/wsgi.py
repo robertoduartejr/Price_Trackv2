5c3da9ee1b32edca9cf968f21c4cd4bbf44c4af6
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-05 18:05:34 -0300

    final project heroku ready teste10

--- project/wsgi.py
+++ project/wsgi.py
@@ -6,29 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-import threading
+
 import os
-from apscheduler.schedulers.blocking import BlockingScheduler
-from track.models import Track
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from datetime import datetime
-#from backendfunctions import send_email_price,scrapingscheduler,scraping,updatescraping
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-#in order to run selenium on heroku
-
-
-
-
-#SOLUTION TO RUN CODE SHOULD BE WRITTEN HERE
-
-
-#thread = threading.Thread(target=scrapingscheduler)
-#thread.start()
-#SOLUTION TO RUN CODE SHOULD BE WRITTEN HERE
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
