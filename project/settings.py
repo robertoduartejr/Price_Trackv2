0f33300ace136c00433893a3ed34de25086be182
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-05 17:49:42 -0300

    final project heroku ready teste8

--- project/settings.py
+++ project/settings.py
@@ -140,7 +140,7 @@ LOGOUT_REDIRECT_URL = '/main'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
