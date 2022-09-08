f6b1917be176cec796ce89c078f9b6cd06ccbc36
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-08 20:25:08 -0300

    final version database changed to heroku v8

--- backendfunctions.py
+++ backendfunctions.py
@@ -22,7 +22,12 @@ def send_email_price(email,desired_price, price, name,user,link):
     send_mail(f'{name} below R$ {desired_price}!!! ',f'Dear, {user}.\n\nThe price of {name} is R$ {price}.\n\nIt is below the one you set as desired (R$ {desired_price})\n\nI suggest you run in order not to miss the chance. \n\n Item link: {link}\n\nAtt, Track Price team.','trackpricedjango@gmail.com', [email], fail_silently=False)
 
 def scraping(link): #function to perform scrapping
-    navegador = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     navegador.get(link)
     try:
         if 'amazon.com.br/' in link:
@@ -91,7 +96,7 @@ def waiting():
 
 def scrapingscheduler():  #function to run the scraping in intervals
     scheduler = BlockingScheduler()
-    scheduler.add_job(lambda: updatescraping(Track), 'interval', seconds=300)
+    scheduler.add_job(lambda: updatescraping(Track), 'interval', hours=2)
     scheduler.start()
 
 # def scrapingscheduler2():  # function to run the scraping in intervals
