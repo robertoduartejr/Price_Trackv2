fe9a3029b7352237be5370e0252e0ec38ffa61ec
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-08 15:19:46 -0300

    final version database changed to heroku v3

--- project/wsgi.py
+++ project/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-import threading
-from backendfunctions import scrapingscheduler
-thread = threading.Thread(target=scrapingscheduler)
-thread.start()
-#
+# import threading
+# from backendfunctions import scrapingscheduler
+# thread = threading.Thread(target=scrapingscheduler)
+# thread.start()
+# #
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
