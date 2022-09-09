90bca81b13d91f80fb7ffe46208910b66809920c
Roberto Duarte <103781917+robertoduartejr@users.noreply.github.com>
2022-09-08 21:19:24 -0300

    final version to try run webscrapping on heroku

--- main/apps.py
+++ main/apps.py
@@ -11,10 +11,10 @@ class MainConfig(AppConfig):
 # SOLUTION TO RUN CODE, HOWEVER I'M USING ANOTHER SOLUTION AT WSGI FILE
 
     # pra fazer funcionar é só tirar os comentarios
-    # def ready(self):
-    #     from backendfunctions import scrapingscheduler
-    #     import threading
-    #     from track.models import Track
-    #     from .models import CustomUser
-    #     thread = threading.Thread(target=scrapingscheduler)
-    #     thread.start()
+    def ready(self):
+        from backendfunctions import scrapingscheduler
+        import threading
+        from track.models import Track
+        from .models import CustomUser
+        thread = threading.Thread(target=scrapingscheduler)
+        thread.start()
